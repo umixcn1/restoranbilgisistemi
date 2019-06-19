fc15041824dda5ad7f89c32dea0e426b457be6eb
umixcn1 <altunumitcan@gmail.com>
2019-06-20 02:36:43 +0300

    düzenleme

--- OnlineLokanta/pages/siparis.aspx.cs
+++ OnlineLokanta/pages/siparis.aspx.cs
@@ -9,20 +9,35 @@ using System.Data.SqlClient;
 using System.Web.Configuration;
 
 namespace OnlineLokanta.pages
+
 {
-    public partial class siparis : System.Web.UI.Page
+
+    public partial class siparis : System.Web.UI.Page {
+
             
-    {
+    
         string connStr = WebConfigurationManager.ConnectionStrings["DBConn"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
+       
         protected void yerat_Click(object sender, EventArgs e)
         {
             SqlConnection conn = new SqlConnection(connStr);
-            SqlCommand cmd = new SqlCommand("sp_re", conn);
+            SqlCommand cmd = new SqlCommand("insert into tblrezervasyon values(@kisiID,satisID,@tarihsaat,@masaTercihi)", conn);
+            cmd.Parameters.AddWithValue("@kisiID", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@satisID", TextBox2.Text);
+
+            cmd.Parameters.AddWithValue("@tarihsaat", Calendar1.SelectedDate);
+            cmd.Parameters.AddWithValue("@masaTercihi", RadioButtonList1.SelectedItem);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+
+            conn.Open();
+
         }
     }
 }
